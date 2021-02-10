6f5ad0cd6823ab1555e1a7c0008947d15b5a28de
Hugo Baret <hugo.baret@epitech.eu>
2021-02-10 11:38:28 +0400

    2

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <div id="app">
+    <HeaderBar/>
     <router-view/>
   </div>
 </template>
 
+<script>
+import HeaderBar from './components/HeaderBar'
+export default {
+  components: {
+    HeaderBar
+  },
+}
+</script>
+
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ front/src/components/HeaderBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="div">
+        <h1 class="h1">
+            PostApp
+        </h1>
+        <h2 class="h2">
+            Home Login Signup
+        </h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    .h1 {
+        ;
+        text-indent: -40cm;
+        font-family: 'Lobster', cursive;
+        color: #FBFBFB;
+    }
+    .h2 {
+        word-spacing: 1.5cm;
+        font-family: 'Roboto', sans-serif;
+        color: #FBFBFB;
+    }
+    .div {
+        background-color: #45AAF2;
+    }
+</style>
